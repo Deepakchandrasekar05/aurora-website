a37e1ced46337bbb2dce4108f6208b4af4adb528
Risheekesh K G <rishikesh9894318193@gmail.com>
2025-02-24 19:15:49 +0530

    Intialized Routes,Pages

---
+++ Frontend/src/pages/home.jsx
@@ -0,0 +1,11 @@
+const Home = () => {
+    return (
+      <div>
+        <h1>Aurora Website </h1>
+        <p>lorem ipsum </p>
+      </div>
+    );
+  };
+  
+  export default Home;
+  
---
+++ Frontend/src/pages/sponsership.jsx
@@ -0,0 +1,10 @@
+const Sponsership = () => {
+    return (
+      <div>
+        <h1>Welcome to the Rover Team</h1>
+        <p>Exploring space, one step at a time.</p>
+      </div>
+    );
+  };
+  
+  export default Sponsership;
